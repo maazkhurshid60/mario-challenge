f2482e227dfedae136605334e15126e8e996d126
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-05-28 18:16:52 +0500

    Fixing issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mario-challenge/' // Comment this line when running on the local server
+  // base: '/mario-challenge/' // Comment this line when running on the local server
 })
