3cff6ed5f6355796fe8323490852db6ae2602be1
maazkhurshid60 <maaz.khurshid60@gmail.com>
2024-07-11 23:07:23 +0500

    adding line

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import router from './routes/router.jsx';
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // Removing this line for git activity 
+  // Adding this line for git activity 
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
