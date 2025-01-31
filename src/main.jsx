8517e67a32bfc649766a1b62ffd65003db06e0d3
maazkhurshid60 <maaz.khurshid60@gmail.com>
2025-01-31 20:45:52 +0500

    git activity performed

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { RouterProvider } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
 
 
-  //Deleting Line this line for git activity 
+  //Adding Line this line for git activity 
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
